360d13624721b34508e9b82b9ec467c1ff44391b
tomek401273 <tomek371240@gmail.com>
2018-01-23 23:09:42 +0100

    firstCommit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/task/';
+  var apiRoot = 'https://fast-lowlands-46997.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
